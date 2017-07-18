79ec08b42c42548770e9492f4de83ad369079ac5
Sokly <sokly@yoolk.com>
2017-07-18 10:11:04 +0700

    chore(): set up .env for migration

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,8 @@ require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
+# Bundler.require(*Rails.groups)
+Bundler.require(:default, Rails.env)
 ENV.update YAML.load_file('config/setting.yml')[Rails.env] rescue {}
 
 module Gogreen
