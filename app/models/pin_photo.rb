516c4ed7ea93e6586781898622e45b85a44b9e21
Sokly <sokly@yoolk.com>
2017-07-24 09:29:51 +0700

    chore(): add annotate gem

--- app/models/pin_photo.rb
+++ app/models/pin_photo.rb
@@ -1,3 +1,12 @@
+# == Schema Information
+#
+# Table name: pin_photos
+#
+#  id          :integer          not null, primary key
+#  pin_id      :integer
+#  is_approved :boolean          default(FALSE)
+#
+
 class PinPhoto < ApplicationRecord
   belongs_to :pin
   mount_base64_uploader :photo, ImageUploader
